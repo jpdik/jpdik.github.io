295d43a03d195d2bd38829eaf0e8a5ceb32e6414
jpdik <jpdik@users.noreply.github.com>
2024-02-14 22:49:39 +0000

    Build: (refs/heads/master) 93f7bff98f2589df2b010d53da1af0e5e7bb5ae8

---
+++ assets/index-iHss4ZcY.js
@@ -0,0 +1,24 @@
+import{u as r,a as t,j as e}from"./index-jfjmIP0p.js";import{H as n}from"./Helmet-QhHoocSb.js";const s=r.main`
+  background: #ff3300;
+
+  color: white;
+
+  padding: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 3rem;
+
+  > svg {
+    max-height: 70vh;
+  }
+
+  > h1 {
+    text-align: center;
+    font-size: 4rem;
+    font-style: italic;
+    text-transform: uppercase;
+  }
+`,l=()=>{const o=t();return console.error(o),e.jsxs(s,{children:[e.jsxs(n,{children:[e.jsx("title",{children:"Ocorreu um erro"}),e.jsx("meta",{name:"robots",content:"noindex, nofollow"}),e.jsx("meta",{name:"googlebot",content:"noindex, nofollow"})]}),e.jsx("div",{children:"Erro"})]})};export{l as default};
---
+++ assets/index-o9KXllPM.js
@@ -0,0 +1,28 @@
+import{u as e,j as n}from"./index-jfjmIP0p.js";import{H as t}from"./Helmet-QhHoocSb.js";const o=e.main`
+  background: #ff3300;
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+
+  color: white;
+
+  padding: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 3rem;
+
+  > svg {
+    max-height: 70vh;
+  }
+
+  > h1 {
+    text-align: center;
+    font-size: 4rem;
+    font-style: italic;
+    text-transform: uppercase;
+  }
+`,i=()=>n.jsxs(o,{children:[n.jsxs(t,{children:[n.jsx("title",{children:"Pagina não encontrada"}),n.jsx("meta",{name:"robots",content:"noindex, nofollow"}),n.jsx("meta",{name:"googlebot",content:"noindex, nofollow"})]}),n.jsx("h1",{children:"Pagina não encontrada"})]});export{i as default};
